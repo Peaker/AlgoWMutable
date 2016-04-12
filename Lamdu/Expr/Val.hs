e2c59a2c887c7739d5922d128b86cc2458c7244f
Eyal Lotem <eyal.lotem@gmail.com>
2016-04-12 17:06:10 +0300

    Unpack Body records

--- Lamdu/Expr/Val.hs
+++ Lamdu/Expr/Val.hs
@@ -134,15 +134,15 @@ instance Match Nom where
         | otherwise = Nothing
 
 data Body v
-    = BLam !(Lam v)
-    | BApp !(Apply v)
-    | BRecExtend !(RecExtend v)
-    | BCase !(Case v)
-    | BGetField !(GetField v)
-    | BInject !(Inject v)
-    | BFromNom !(Nom v)
-    | BToNom !(Nom v)
-    | BLeaf !Leaf
+    = BLam       {-# UNPACK #-}!(Lam v)
+    | BApp       {-# UNPACK #-}!(Apply v)
+    | BRecExtend {-# UNPACK #-}!(RecExtend v)
+    | BCase      {-# UNPACK #-}!(Case v)
+    | BGetField  {-# UNPACK #-}!(GetField v)
+    | BInject    {-# UNPACK #-}!(Inject v)
+    | BFromNom   {-# UNPACK #-}!(Nom v)
+    | BToNom     {-# UNPACK #-}!(Nom v)
+    | BLeaf      !Leaf
     deriving (Show, Functor, Foldable, Traversable)
 
 instance NFData v => NFData (Body v) where
